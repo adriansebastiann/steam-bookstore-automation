ac60fe4d4f9847dd0c4caccc87f279fad1665946
Adrian-Sebastian Alexoaie <adrianalexoaie@yahoo.com>
2025-08-24 15:51:25 +0300

    attempt#1 to fix failure on github

--- SteamBookstoreTests/Config/BrowserSettings.cs
+++ SteamBookstoreTests/Config/BrowserSettings.cs
@@ -8,5 +8,10 @@ namespace SteamBookstoreTests.Config
 {
     internal class BrowserSettings
     {
+        public string BrowserName { get; set; } = "chrome";
+        public bool Headless { get; set; } = false;
+        public int ImplicitWaitSeconds { get; set; } = 5;
+        public int PageLoadTimeoutSeconds { get; set; } = 60;
+        public bool Incognito { get; set; } = true;
     }
 }
--- SteamBookstoreTests/Drivers/WebDriverFactory.cs
+++ SteamBookstoreTests/Drivers/WebDriverFactory.cs
@@ -13,11 +13,8 @@ namespace SteamBookstoreTests.Drivers
             options.AddArgument("--disable-gpu");
             options.AddArgument("--no-sandbox");
             options.AddArgument("--start-maximized");
-            if (Environment.GetEnvironmentVariable("HEADLESS") == "1")
-            {
-                options.AddArgument("--headless=new");
-                options.AddArgument("--window-size=1920,1080");
-            }
+            options.AddArgument("--headless=new");
+            options.AddArgument("--window-size=1920,1080");
             var driver = new ChromeDriver(options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
